197100cbe8623f0362c51f86916760666bfd0cb8
Aseem Sahoo <aseemsahoo.v@gmail.com>
2023-09-01 17:20:22 +0530

    improved navigation in home

--- src/components/Home/Navigation/NavigationHome.js
+++ src/components/Home/Navigation/NavigationHome.js
@@ -4,16 +4,17 @@ function NavigationHome() {
   return (
     <nav className="navbar top navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand p-2 m-2">Home navigator </a>
-      <div className="col d-flex mx-4">
+      <div className="container-fluid d-flex mx-4 justify-content-start">
         <input type="text" placeholder="Enter a keyword" />
-        <button> Search </button>
+        <button class="mx-3"> Search </button>
       </div>
+
       <ul class="nav justify-content-end">
         <li class="nav-item mx-4">
-            <a> Home </a>
+          <a> Home </a>
         </li>
         <li class="nav-item mx-4">
-            <a> My Profile </a>
+          <a> My Profile </a>
         </li>
       </ul>
     </nav>

9fb2857c3c9a5f4ae45fd04a61b4cd2c5415cedc
aseemsahoo <aseemsahoo.v@gmail.com>
2023-12-04 14:56:33 +0530

    feature: now App can detect token validity on launch

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import "./App.css";
 import Form from "./components/Forms/form/Form";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Routes, useNavigate } from "react-router-dom";
+
 import Home from "./components/Home/home/Home";
 import LoginForm from "./components/Forms/login-form/LoginForm";
 import SignupForm from "./components/Forms/signup-form/SignupForm";
 import StockChart from "./components/Home/Stocks/stock-chart/StockChart";
 import Stock from "./components/Home/Stocks/stock/Stock";
 import StocksList from "./components/Home/Stocks/stocks-list/StocksList";
-import Profile from "./components/Profile/profile/Profile";
+import Profile from "./components/Profile/profile/Profile"
+import { useState, useEffect } from "react";
+import AuthService from "./services/AuthService";
 
-function App() {
+const App = () => {
+  const navigate = useNavigate();
+
+  // Check token expiration on app launch
+  useEffect(() => {
+    if (AuthService.isTokenExpired()) {
+      
+      alert('Token invalid or missing. Redirecting to login page...');
+      AuthService.removeToken();
+      navigate('/login'); 
+    }
+
+  }, []);
+
   return (
-    // define all the routes here
     <Routes>
       <Route path="/" element={<Form />}>
         <Route path="/login" element={<LoginForm />} />
@@ -25,6 +40,10 @@ function App() {
       <Route path="/profile" element={<Profile />} />
     </Routes>
   );
-}
+};
+
+// function App() {
+  
+// }
 
 export default App;
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,14 +1,43 @@
-let TOKEN = 'access_token';
+import { isExpired, decodeToken } from "react-jwt";
 
 const AuthService = 
 {
-  getToken: () => localStorage.getItem(TOKEN),
+  getToken: () => localStorage.getItem("TOKEN"),
 
-  setToken: (token) => localStorage.setItem(TOKEN, token),
+  setToken: (token) => localStorage.setItem("TOKEN", token),
 
-  removeToken: () => localStorage.removeItem(TOKEN),
+  removeToken: () => localStorage.removeItem("TOKEN"),
 
   isAuthenticated: () => !!AuthService.getToken(),
+
+  isTokenExpired: () => {
+    const token = AuthService.getToken();
+
+    const DecodedToken = decodeToken(token);
+    const isTokenExpired = isExpired(token);  
+    
+    return isTokenExpired;
+
+    // if (isTokenExpired) {
+    //   try {
+            // jsonwebtoken doesnt work with current version
+    //     // const decodedToken = jwt.decodeToken();
+
+    //     // if (decodedToken && decodedToken.exp * 1000 < Date.now()) { // Token has expired
+    //       console.log('Token expired. Removed from local storage.');
+    //       return false;
+    //     }
+
+    //     return true;
+    //   } 
+    //   catch (error) {
+    //     console.error('Error decoding token:', error);
+    //     return false;
+    //   }
+    // }
+    // // No token found
+    // return false;
+  },
 };
 
 export default AuthService;
